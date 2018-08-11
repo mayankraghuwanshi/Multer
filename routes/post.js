411b8461eaaa3658fba96f6d35e43241b4715342
meraghuwanshi <mayank1272@hotmail.com>
2018-08-11 17:00:50 +0530

    Getting uploaded file in image folder.

--- routes/post.js
+++ routes/post.js
@@ -3,24 +3,25 @@ const mongoose = require('mongoose');
 const Post = require('../modules/post');
 const multer = require('multer');
 const upload = multer({
-    dest:"images/"
+    dest:"../images/"
 })
+
 mongoose.connect('mongodb://localhost:27017')
 
 route.get('/',(req , res)=>{
+
     Post.find({}).then((data)=>{
         res.send(data)
     }).catch((err)=>{
         res.send({error:err})
     })
 })
-route.post('/',upload.single("productImage"),( req , res)=>{
-     console.log(req.file)
-
-    const post = new Post({title:req.body.title})
+route.post('/post',upload.single("image"),( req , res)=>{
+   /* const post = new Post({title:req.body.title})
     post.save().then((data)=>{
         res.send(data)
-    })
+    })*/
+   res.send(req.file)
 
 
 
--- server.js
+++ server.js
@@ -5,9 +5,9 @@ server.use(express.json());
 server.use(express.urlencoded({
     extend:false
 }))
-server.use('/uploads', express.static('images'));
+server.use(express.static('images'));
 server.use('/user',require('./routes/user'))
-server.use('/post',require('./routes/post'))
+server.use('/',require('./routes/post'))
 
 
 
